931ab8e5c4c3efe0f95b59af8218134dbc27b8a6
Kai Li <79342231+kailathan@users.noreply.github.com>
2024-09-19 19:46:28 -0400

    Update train_bt.sh

--- pretraining/train_bt.sh
+++ pretraining/train_bt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=BT_real
+#SBATCH --job-name=BT
 #SBATCH --cpus-per-task=4
 #SBATCH --gpus-per-node v100:8
 #SBATCH --nodes=1
@@ -10,10 +10,10 @@
 #SBATCH --error=BT1/BT_randstain_error_%j.txt
 #SBATCH --mem=250G
 
-#SBATCH --account=def-ravanelm
+#SBATCH --account=acc_name
 module load opencv
 nvidia-smi 
-source ~/scratch/ENV/bin/activate 
+source vmamba/bin/activate 
 python train.py /path/to/dataset
   --cfg /configs/vssm/vmambav0_base_224.yaml \
   --workers 8 \
